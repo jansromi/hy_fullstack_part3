187d4b450472d6551696438d0517f185e5681037
jansromi <jansromi@student.jyu.fi>
2024-01-03 16:09:17 +0200

    teht 3.3-3.4

--- index.js
+++ index.js
@@ -5,22 +5,53 @@ const app = express();
 app.use(express.json());
 
 
-const persons = require('./persons');
+let persons = require('./persons');
 
 
 app.get('/', (req, res) => {
     res.sendStatus(200);
 })
 
+/**
+ * Route providing all entries
+ */
 app.get('/api/persons', (req, res) => {
     res.json(persons);
 })
 
+/**
+ * Route providing a single entry
+ */
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+})
+
+/**
+ * Route for info-page
+ */
 app.get('/info', (req, res) => {
+    // send how many entries and current date
     res.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date().toString()}</p>
     `);
+})
+
+/**
+ * Route for deleting an entry
+ */
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    // filter the array to return a new array without the deleted entry
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
 })
 
 const PORT = 3001
