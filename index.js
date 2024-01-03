7eed23fc464494700beee77d02f54ec8dcfe7ae8
jansromi <jansromi@student.jyu.fi>
2024-01-03 15:48:25 +0200

    teht3.2

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // use json parser
-app.use(express.json())
+app.use(express.json());
 
-const persons = require('./persons')
+
+const persons = require('./persons');
+
 
 app.get('/', (req, res) => {
     res.sendStatus(200);
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
-  })
+    res.json(persons);
+})
+
+app.get('/info', (req, res) => {
+    res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date().toString()}</p>
+    `);
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
