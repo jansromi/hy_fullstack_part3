f43edfcf827f4d82d274a1b6691f84a23acfdfbd
jansromi <jansromi@student.jyu.fi>
2024-02-23 11:26:17 +0200

    3.19-3.21

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
     }
-  
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+    
     next(error)
   }
 
@@ -94,17 +97,17 @@ app.delete('/api/persons/:id', (req, res, next) => {
  * 
  * @returns the modified resource
  */
-app.put('/api/persons/:id', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
+    const { name, number } = req.body;
     const body = req.body;
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    Person.findByIdAndUpdate(
+        req.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query'}
+    )
     .then(updatedPerson => {
-        res.json(updatedPerson.toJSON())
+        res.json(updatedPerson)
     })
     .catch(error => next(error))
 })
@@ -112,7 +115,7 @@ app.put('/api/persons/:id', (req, res) => {
 /**
  * Route for adding a new entry
  */
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body;
 
     // if name is missing
@@ -142,10 +145,11 @@ app.post('/api/persons', (req, res) => {
             number: body.number,
         })
     
-        person.save().then(savedPerson => {
-            persons = persons.concat(savedPerson)
+        person.save()
+        .then(savedPerson => {
             res.json(savedPerson)
         })
+        .catch(error => next(error))
     })
 })
 
--- models/person.js
+++ models/person.js
@@ -14,8 +14,22 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    number: {
+        type: String,
+        required: true,
+        minlength: 8,
+        validate: {
+          validator: function(v) {
+              return /^(\d{2,3}-\d{6,})$/.test(v);
+          },
+          message: props => `${props.value} is not a valid phone number!`
+      }
+    }
   })
 
 // define toJSON method
