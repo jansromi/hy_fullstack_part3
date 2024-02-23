513c432387eb80f197a5efa8748c4dfb6f2ca899
jansromi <jansromi@student.jyu.fi>
2024-02-23 11:42:15 +0200

    linted code, teht. 3.22

--- models/person.js
+++ models/person.js
@@ -6,12 +6,12 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then((result) => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
-  });
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -25,12 +25,12 @@ const personSchema = new mongoose.Schema({
     minlength: 8,
     validate: {
       validator: function(v) {
-        return /^(\d{2,3}-\d{6,})$/.test(v);
+        return /^(\d{2,3}-\d{6,})$/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
   },
-});
+})
 
 // define toJSON method
 personSchema.set('toJSON', {
@@ -41,4 +41,4 @@ personSchema.set('toJSON', {
   },
 })
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
